229244b8bc956e557983f7ac35287d7a1ec6e88d
Vasivvv <tu4ev0bpe@code.edu.az>
2022-11-01 01:58:59 +0400

    Loops tasks done

---
+++ Task 1,2,3.cs
@@ -0,0 +1,63 @@
+﻿#region
+//int eded, sum = 0;
+//Console.Write("Ededi daxil edin:");
+//eded = Convert.ToInt32(Console.ReadLine());
+//for (int i = 2; i <= eded; i *= 2)
+//{
+
+//    if (i % 2 == 0)
+//        sum++;
+//}
+//Console.WriteLine(sum);
+//if ((Math.Pow(2, sum + 1)) - eded < eded - (Math.Pow(2, sum)))
+//{
+//    Console.WriteLine(sum + 1);
+//}
+//if ((Math.Pow(2, sum + 1)) - eded >= eded - (Math.Pow(2, sum)))
+//{
+//    Console.WriteLine(sum);
+//}
+#endregion
+
+#region task 2
+//int eded, sum = 0;
+//Console.Write("Ededi daxil edin:");
+//eded = Convert.ToInt32(Console.ReadLine());
+
+//if (eded > 0)
+//{
+
+//    while (eded > 0)
+//    {
+//        sum += eded % 10;
+//        eded = eded / 10;
+//    }
+//    Console.WriteLine(sum);
+
+//}
+#endregion
+
+#region task 3
+//int eded;
+//Console.Write("Ededi daxil edin:");
+//eded = Convert.ToInt32(Console.ReadLine());
+//if (eded == 0 || eded == 1)
+//{
+//    Console.WriteLine(eded + " murekkebdir");
+//    Console.ReadLine();
+//}
+//else
+//{
+//    for (int i = 2; i <= eded / 2; i++)
+//    {
+//        if (eded % i == 0)
+//        {
+//            Console.WriteLine(eded + " murekkebdir");
+//            return;
+//        }
+
+//    }
+//    Console.WriteLine(eded + " sadedir");
+//    Console.ReadLine();
+//}
+#endregion
